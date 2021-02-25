721a9413900d999dfcdbc44c49f466b68b73e4bf
11k1th <76481216+11k1th@users.noreply.github.com>
2021-02-25 19:59:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,94 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine,world;
+var ground, hero, link, monster, a;
+var b1, b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b19,b20
+
+function preload() {
+//preload the images here
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+
+    engine = Engine.create();
+    world = engine.world
+
+    ground = new Ground(600,600,1200,20);
+
+
+    monster = new Monster(900, 300, 120,120);
+
+    b1 = new Box(840,100,50,50);
+    b2 = new Box(840,100,50,50);
+    b3 = new Box(840,100,50,50);
+    b4 = new Box(840,100,50,50);
+    b5 = new Box(840,100,50,50);
+    b6 = new Box(840,100,50,50);
+    b7 = new Box(770,100,50,50);
+    b8 = new Box(770,100,50,50);
+    b9 = new Box(770,100,50,50);
+    b10 = new Box(770,100,50,50);
+    b11 = new Box(770,100,50,50);
+    b12 = new Box(770,100,50,50);
+    b13 = new Box(700,100,50,50);
+    b14 = new Box(700,100,50,50);
+    b15 = new Box(700,100,50,50);
+    b16 = new Box(700,100,50,50);
+    b17 = new Box(700,100,50,50);
+    b18 = new Box(700,100,50,50);
+    b19 = new Box(700,100,50,50);
+    b20 = new Box(700,100,50,50);
+
+    hero = new Hero(200,200,80,80);
+
+    link = new Link(hero.body, {x: 500, y: 50} )
+
+}
+
+function draw() {
+  background(0);
+
+    Engine.update(engine);
+
+    ground.display();
+
+    b1.display();
+    b2.display();
+    b3.display();
+    b4.display();
+    b5.display();
+    b6.display();
+    b7.display();
+    b8.display();
+    b9.display();
+    b10.display();
+    b11.display();
+    b12.display();
+    b13.display();
+    b14.display();
+    b15.display();
+    b16.display();
+    b17.display();
+    b18.display();
+    b19.display();
+    b20.display();
+
+    monster.display();
+
+    hero.display();
+}
+
+function mouseDragged() {
+
+    Matter.Body.setPosition(hero.body, {x:mouseX, y:mouseY} );
+
+}
+
+//}
+
